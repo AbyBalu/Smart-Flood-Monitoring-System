ded4ab030f981402f6420d7bfc49671e9678486c
Aby B <abybalu@gmail.com>
2020-02-13 22:36:15 +0530

    Add files via upload

---
+++ Flood_Monitro_node_1/Flood_Monitro_node_1.ino
@@ -0,0 +1,118 @@
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
+#include <DHT.h>               // including the library of DHT11 temperature and humidity sensor
+#define BLYNK_PRINT Serial
+#define DHTTYPE DHT11          // DHT 11
+#define dht_dpin 3             //RX, defines pin number
+
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+DHT dht(dht_dpin, DHTTYPE);
+WidgetLCD lcd1(V1);
+WidgetBridge bridge1(V1);
+
+BLYNK_CONNECTED() {
+  // Place the AuthToken of the second hardware here
+  bridge1.setAuthToken("f2a44ecc55f64a98906eba3d0542f8d7"); 
+}
+
+
+char auth[] = "acd7381d85564370ab9699190f9e9a6b";       // You should get Auth Token from the Blynk App
+char ssid[] = "Redmi";        // Your WiFi credentials.
+char pass[] = "1234567890";   // Set password to "" for open networks.
+const int trigPin = D2;       // defines pin number
+const int echoPin = D1;       // defines pin number
+const int input = D7;         // defines pin number
+long duration;                // defines variables
+int distance;                 // defines variables
+
+int X;
+int Y;
+float TIME = 0;
+float FREQUENCY = 0;
+float WATER = 0;
+float TOTAL = 0;
+float LS = 0;
+
+void setup() 
+{
+ Blynk.begin(auth, ssid, pass);
+ pinMode(trigPin, OUTPUT);    // Sets the trigPin as an Output
+ pinMode(echoPin, INPUT);     // Sets the echoPin as an Input
+ Serial.begin(9600);          // Starts the serial communication
+ pinMode(input,INPUT);        // defines pin number
+ lcd.backlight();             // Turn on the backlight.
+}
+
+void loop() 
+{
+
+X = pulseIn(input, HIGH);
+Y = pulseIn(input, LOW);
+TIME = X + Y;
+FREQUENCY = 1000000/TIME;
+WATER = FREQUENCY/7.5;
+LS = WATER/60;
+
+  
+ lcd1.clear();
+ lcd1.print(0, 0, "Distance: ");        // use: (position X: 0-15, position Y: 0-1, "Message you want to print")
+ digitalWrite(trigPin, LOW);                // Clears the trigPin
+ delayMicroseconds(2);
+ digitalWrite(trigPin, HIGH);               // Sets the trigPin on HIGH state for 10 micro seconds
+ delayMicroseconds(10);
+ digitalWrite(trigPin, LOW);
+ duration = pulseIn(echoPin, HIGH);         // Reads the echoPin, returns the sound wave travel time in microseconds
+ distance= duration*0.034/2;                // Calculating the distance
+ bridge1.virtualWrite(V1, distance); 
+ Serial.print("Distance: ");                // Prints the distance on the Serial Monitor
+ Serial.println(distance);
+ Wire.begin(D4, D3);                        //Use predefined PINS consts
+ lcd.begin();                               // The begin call takes the width and height. This should match the number provided to the constructor.
+ lcd.home();
+ lcd.setCursor(0, 0);                       // Move the cursor at origin
+ lcd.print("Distance:");
+ lcd.print(distance);
+ lcd1.print(6, 1, distance);
+ lcd1.print(10, 1, "cm");
+ dht.begin();
+ float h = dht.readHumidity();              // get readings from the DHT11 sensor
+ float t = dht.readTemperature();           // get readings from the DHT11 sensor
+ Blynk.virtualWrite(V3, h);                 // Send humidity to Blynk App. Display on Gauge Widget
+ Blynk.virtualWrite(V2, t);                 // Send temperature to Blynk App. Display on Gauge Widget
+ lcd.setCursor(0, 1);     
+ lcd.print("Humidity:");
+ lcd.print(h);
+ lcd.print("%");
+ lcd.setCursor(0, 2);
+ lcd.print("Temperature:");
+ lcd.print(t); 
+ lcd.println("C");
+
+if(FREQUENCY >= 0)
+{
+if(isinf(FREQUENCY))
+{
+lcd.setCursor(0,3);
+lcd.print("VOL. :0.00");
+lcd.print(" L/M");
+Blynk.virtualWrite(V4, 0);
+bridge1.virtualWrite(V4, 0); 
+}
+else
+{
+TOTAL = TOTAL + LS;
+Serial.println(FREQUENCY);
+Blynk.virtualWrite(V4, WATER);
+bridge1.virtualWrite(V4, WATER); 
+lcd.setCursor(0,3);
+lcd.print("VOL. :");
+lcd.print(WATER);
+lcd.print(" L/M");
+}
+}
+ 
+ Blynk.run();
+ delay(3000); 
+}
---
+++ Flood_Monitro_node_2/Flood_Monitro_node_2.ino
@@ -0,0 +1,172 @@
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(D1,D2);
+
+char auth[] = "f2a44ecc55f64a98906eba3d0542f8d7";   // You should get Auth Token in the Blynk App.
+                                                    // Go to the Project Settings (nut icon).
+char ssid[] = "Redmi";              // Your WiFi credentials.
+char pass[] = "1234567890";         // Set password to "" for open networks.
+int distance;                       // defines variables
+
+BLYNK_WRITE(V1)                     // This code will update the virtual port 1
+{
+  int UData = param.asInt();
+  if(UData<=20)
+  {
+    smsAlert();                     // Function call
+  }
+}
+
+BLYNK_WRITE(V4)                     // This code will update the virtual port 4
+{
+  int WData = param.asInt();
+  if(WData>=5)
+  {
+    smsWarning();                   // Function call
+  }
+}
+
+void setup()
+{
+  mySerial.begin(9600);             // Setting the baud rate of GSM Module  
+  Serial.begin(9600);               // Starts the serial communication
+  Blynk.begin(auth, ssid, pass);
+  pinMode(D3, OUTPUT);              // defines pin number
+  pinMode(D4, OUTPUT);              // defines pin number
+  pinMode(D5, OUTPUT);              // defines pin number
+}
+
+void loop()
+{
+  Blynk.run();
+}
+
+void smsWarning()
+{
+  Serial.print("Warning");
+  digitalWrite(D5, HIGH);            // turn the LED on (HIGH is the voltage level)                   
+  smsWarning1();
+  smsWarning2();
+  smsWarning3();
+  smsWarning4();
+  digitalWrite(D5, LOW);                            // turn the LED off by making the voltage LOW
+
+}
+
+void smsAlert()
+{
+  Serial.print("Alert");
+  digitalWrite(D3, HIGH);                           // turn the Buzzer on (HIGH is the voltage level)
+  digitalWrite(D4, HIGH);                           // turn the LED on (HIGH is the voltage level)
+  smsAlert1();
+  smsAlert2();
+  smsAlert3();
+  smsAlert4();
+  digitalWrite(D3, LOW);                            // turn the Buzzer off by making the voltage LOW
+  digitalWrite(D4, LOW);                            // turn the LED off by making the voltage LOW
+}
+
+void smsWarning1()
+{
+  mySerial.println("AT+CMGF=1");     //Sets the GSM Module in Text Mode
+  delay(1000);                                      // wait for a second
+  mySerial.println("AT+CMGS=\"+917994730902\"\r");  // Replace x with mobile number
+  delay(500); 
+  mySerial.println("!!!!FLOOD WARNING!!!!   Move to Area 1 ");        // The SMS text you want to send
+  delay(100);
+  mySerial.println((char)26);                       // ASCII code of CTRL+Z for saying the end of sms to  the module 
+  delay(500); 
+}
+
+void smsWarning2()
+{
+  mySerial.println("AT+CMGF=1");     //Sets the GSM Module in Text Mode
+  delay(1000);                                      // wait for a second
+  mySerial.println("AT+CMGS=\"+918848723212\"\r");  // Replace x with mobile number
+  delay(500); 
+  mySerial.println("!!!!FLOOD WARNING!!!!   Move to Area 1 ");        // The SMS text you want to send
+  delay(100);
+  mySerial.println((char)26);                       // ASCII code of CTRL+Z for saying the end of sms to  the module 
+  delay(500);
+}
+
+
+void smsWarning3()
+{
+  mySerial.println("AT+CMGF=1");     //Sets the GSM Module in Text Mode
+  delay(1000);                                      // wait for a second
+  mySerial.println("AT+CMGS=\"+917356021996\"\r");  // Replace x with mobile number
+  delay(500); 
+  mySerial.println("!!!!FLOOD WARNING!!!!   Move to Area 1 ");        // The SMS text you want to send
+  delay(100);
+  mySerial.println((char)26);                       // ASCII code of CTRL+Z for saying the end of sms to  the module 
+  delay(500);
+}
+
+
+void smsWarning4()
+{
+
+  mySerial.println("AT+CMGF=1");     //Sets the GSM Module in Text Mode
+  delay(1000);                                      // wait for a second
+  mySerial.println("AT+CMGS=\"+919496208268\"\r");  // Replace x with mobile number
+  delay(500);                                       // wait for a millisecond
+  mySerial.println("!!!!FLOOD WARNING!!!!   Move to Area 1 ");        // The SMS text you want to send
+  delay(100);
+  mySerial.println((char)26);                       // ASCII code of CTRL+Z for saying the end of sms to  the module 
+  delay(500); 
+}
+
+
+void smsAlert1()
+  {
+  mySerial.println("AT+CMGF=1");                    //Sets the GSM Module in Text Mode
+  delay(1000);                                      // wait for a millisecond
+  mySerial.println("AT+CMGS=\"+917994730902\"\r");  // Replace x with mobile number
+  delay(500); 
+  mySerial.println("!!!!FLOOD ALERT!!!!  !!!URGENT!!!   Move to Area 2");          // The SMS text you want to send
+  delay(100);
+  mySerial.println((char)26);                       // ASCII code of CTRL+Z for saying the end of sms to  the module 
+  delay(500); 
+  }
+
+void smsAlert2()
+  {
+  mySerial.println("AT+CMGF=1");     //Sets the GSM Module in Text Mode
+  delay(1000);                                      // wait for a second
+  mySerial.println("AT+CMGS=\"+918848723212\"\r");  // Replace x with mobile number
+  delay(500); 
+  mySerial.println("!!!!FLOOD ALERT!!!!  !!!URGENT!!!   Move to Area 2");          // The SMS text you want to send
+  delay(100);
+  mySerial.println((char)26);                       // ASCII code of CTRL+Z for saying the end of sms to  the module 
+  delay(500); 
+  }
+
+void smsAlert3()
+  {
+  mySerial.println("AT+CMGF=1");                    //Sets the GSM Module in Text Mode
+  delay(1000);                                      // wait for a second
+  mySerial.println("AT+CMGS=\"+917356021996\"\r");  // Replace x with mobile number
+  delay(500); 
+  mySerial.println("!!!!FLOOD ALERT!!!!  !!!URGENT!!!   Move to Area 2");          // The SMS text you want to send
+  delay(100);
+  mySerial.println((char)26);                       // ASCII code of CTRL+Z for saying the end of sms to  the module 
+  delay(500); 
+  }
+
+void smsAlert4()
+  {
+  mySerial.println("AT+CMGF=1");     //Sets the GSM Module in Text Mode
+  delay(1000);                                      // wait for a second
+  mySerial.println("AT+CMGS=\"+919496208268\"\r");  // Replace x with mobile number
+  delay(500);                                      // wait for a second
+  mySerial.println("!!!!FLOOD ALERT!!!!  !!!URGENT!!!   Move to Area 2");          // The SMS text you want to send
+  delay(100);
+  mySerial.println((char)26);                       // ASCII code of CTRL+Z for saying the end of sms to  the module 
+  delay(500);                                      // wait for a second
+
+  }
+
